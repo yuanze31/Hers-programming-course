79935c3a85c10b8f749c4a428290e9c5febe65c4
yuanze31 <1538981620@qq.com>
2025-08-16 23:24:57 +0800

    Python-2

---
+++ Python3/2.py
@@ -0,0 +1,11 @@
+print("整数")
+Aint1 = 3
+Aint2 = 1
+print("这是整数", Aint1, Aint2)
+
+print("浮点数")
+Afloat1 = 1.0
+Afloat2 = 0.1
+Afloat3 = 1.0E-31  # 10的负31次方
+print("这是浮点数", Afloat1, Afloat2, Afloat3)
+print("精度不足", Afloat1 + Afloat2, Afloat1 + Afloat3)
